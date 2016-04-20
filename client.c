dd2e9060a15b40c111de8e8deeef642239a02129
Trotter Andrew James <t5trotte@cdf.toronto.edu>
2016-04-20 16:18:33 -0400

    Added a basic deck initialization function.

--- client.c
+++ client.c
@@ -1,7 +1,23 @@
+#include "deck.h"
+
 #include <stdio.h>
 #include <unistd.h>
 
+int initialize_deck(Deck * d)
+{
+	int i;
+	for (i = 0; i < BASE_SIZE; i++)
+	{
+		d->cards[i].id = i;
+		d->cards[i].val = 0;
+	}
+	return(0);
+}
+
 int main (int argc, char **argv)
 {
+	Deck d;
+	initialize_deck(&d);
+	printf("%d\n", d.cards[4].id);
 	return(0);
 }
--- deck.h
+++ deck.h
@@ -1,7 +1,13 @@
-struct Card
+#define BASE_SIZE 30
+
+typedef struct Card
 {
 	int id;
 	int val;
-}
+} Card;
 
+typedef struct Deck
+{
+	Card cards[BASE_SIZE];
+} Deck;
 
