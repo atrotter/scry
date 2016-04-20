aaaa5f988cb7d673f71d602cd31fb5655a4a5d91
Trotter Andrew James <t5trotte@cdf.toronto.edu>
2016-04-20 17:17:15 -0400

    Added code to test deck filling

--- deck.c
+++ deck.c
@@ -41,6 +41,16 @@ int initialize_deck(Deck * d)
 	return(0);
 }
 
+int fill_deck_list(Deck * d, int * list)
+{
+	int i;
+	for (i = 0; i < BASE_SIZE; i++)
+	{
+		add_card(d, list[i]);
+	}
+	return(0);
+}
+
 int fill_deck_random(Deck * d)
 {
 	int i;
@@ -57,11 +67,24 @@ int fill_deck_random(Deck * d)
 
 int main (int argc, char **argv)
 {
+	//Testing
 	Deck d;
 	initialize_deck(&d);
 	srand(time(NULL));
 	fill_deck_random(&d);
 	printf("Size is %d.\n", d.cur_size);
 	printf("First two cards are %d, %d.\n", d.cards[0].id, d.cards[1].id);
+	
+	int a[BASE_SIZE];
+	int i;
+	for (i = 0; i < BASE_SIZE; i++)
+	{
+		a[i] = rand();
+	}
+	Deck d2;
+	d2.cur_size = 0;
+	fill_deck_list(&d2, a);
+	printf("Size is %d.\n", d2.cur_size);
+	printf("First two cards are %d, %d.\n", d2.cards[0].id, d2.cards[1].id);
 	return(0);
 }
